95e7c4c6521c59f4efc1428db9745548c259e831
truetamtam <truetamtam@yandex.ru>
2015-05-26 17:01:43 +0400

    State code.

---
+++ src/com/truetamtam/state_origin/State.java
@@ -0,0 +1,9 @@
+package com.truetamtam.state_origin;
+
+public interface State {
+ 
+	public void insertQuarter();
+	public void ejectQuarter();
+	public void turnCrank();
+	public void dispense();
+}
